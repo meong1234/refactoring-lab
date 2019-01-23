df16539b9ca83a9b2a7a9973b62959df7d8b8dfa
qbl <farabi.iqbal@gmail.com>
2019-01-24 01:51:04 +0700

    Moved method amount_for from Customer to Rental and changed its name to charge

--- chapter-1/lib/customer.rb
+++ chapter-1/lib/customer.rb
@@ -15,7 +15,7 @@ module RentalApp
       total_amount, frequent_renter_points = 0, 0
       result = "Rental Record for #{@name}\n"
       @rentals.each do |element|
-        this_amount = amount_for(element)
+        this_amount = element.charge
 
         # add frequent renter points
         frequent_renter_points += 1
@@ -33,23 +33,6 @@ module RentalApp
       result += "Amount owed is #{total_amount}\n"
       result += "You earned #{frequent_renter_points} frequent renter points"
       result
-    end
-
-    def amount_for(rental)
-      this_amount = 0
-
-      case rental.movie.price_code
-      when Movie::REGULAR
-        this_amount += 2
-        this_amount += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
-      when Movie::NEW_RELEASE
-        this_amount += rental.days_rented * 3
-      when Movie::CHILDRENS
-        this_amount += 1.5
-        this_amount += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
-      end
-
-      this_amount
     end
   end
 end
