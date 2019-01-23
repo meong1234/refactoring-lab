9cf2a192d9afd624180439f11565eb7b798cf5d3
qbl <farabi.iqbal@gmail.com>
2019-01-24 01:08:18 +0700

    Added unit test for new release movie

--- chapter-1/spec/customer_spec.rb
+++ chapter-1/spec/customer_spec.rb
@@ -39,11 +39,39 @@ module RentalApp
             )
           end
         end
-
       end
 
       context 'new release movie' do
-        it 'should return the correct statement result' do
+        let(:movie) { Movie.new('Glass', Movie::NEW_RELEASE) }
+
+        context 'rented days <= 2' do
+          let(:rental) { Rental.new(movie, 1) }
+
+          it 'should return the correct statement result' do
+            customer.add_rental(rental)
+
+            expect(customer.statement).to eq(
+              "Rental Record for Iqbal\n" +
+              "\tGlass\t3\n" +
+              "Amount owed is 3\n" +
+              "You earned 1 frequent renter points"
+            )
+          end
+        end
+
+        context 'rented days > 2' do
+          let(:rental) { Rental.new(movie, 3) }
+
+          it 'should return the correct statement result' do
+            customer.add_rental(rental)
+
+            expect(customer.statement).to eq(
+              "Rental Record for Iqbal\n" +
+              "\tGlass\t9\n" +
+              "Amount owed is 9\n" +
+              "You earned 2 frequent renter points"
+            )
+          end
         end
       end
 
