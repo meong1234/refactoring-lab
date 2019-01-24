5c799ab242fcdaf80dfc6175fbe096401c5901fd
qbl <farabi.iqbal@gmail.com>
2019-01-24 22:49:48 +0700

    Replaced type code with state pattern

---
+++ chapter-1/lib/price.rb
@@ -0,0 +1,24 @@
+module RentalApp
+  class RegularPrice
+    def charge(days_rented)
+      result = 2
+      result += (days_rented - 2) * 1.5 if days_rented > 2
+      result
+    end
+  end
+
+  class NewReleasePrice
+    def charge(days_rented)
+      result = days_rented * 3
+      result
+    end
+  end
+
+  class ChildrensPrice
+    def charge(days_rented)
+      result = 1.5
+      result += (days_rented - 3) * 1.5 if days_rented > 3
+      result
+    end
+  end
+end
